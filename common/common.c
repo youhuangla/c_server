0bdbe9ef557aaa4a8ccfb5c32fa8c9689f849228
youhuangla <1063615427@qq.com>
2023-01-03 11:51:48 +0800

    fcntl

--- common/common.c
+++ common/common.c
@@ -6,6 +6,7 @@
  ************************************************************************/
 
 #include "head.h"
+#include <fcntl.h>
 
 void make_nonblock_ioctl(int fd) {
 	unsigned long ul = 1;
@@ -28,9 +29,43 @@ make_block_ioctl 函数将文件描述符设置为阻塞模式。它的工作原
 */
 
 void make_nonblock(int fd) {
-	fcntl(fd, F_SETFL, O_NONBLOCK);
+	int flag;
+	if ((flag = fcntl(fd, F_GETFL)) < 0) {
+		return ; 
+	}
+	flag |= O_NONBLOCK;
+	fcntl(fd, F_SETFL, flag);
 }
 
+/*
+这是一个函数，用于将文件描述符设置为非阻塞模式。
+
+函数首先使用 fcntl 函数的 F_GETFL 选项来获取文件描述符的当前标志。如果调用失败，则函数返回。
+
+然后，它将 flag 变量与 O_NONBLOCK 常量进行按位或运算。O_NONBLOCK 常量是用于设置文件描述符为非阻塞模式的常量。按位或运算将 flag 变量的 O_NONBLOCK 位设置为 1。
+
+最后，函数使用 fcntl 函数的 F_SETFL 选项将修改后的 flag 值写回文件描述符。这将导致文件描述符被设置为非阻塞模式。
+
+注意：使用 fcntl 函数可能是不可移植的，因此您可能需要使用其他方法来在不同的系统上设置文件描述符的阻塞模式。
+
+*/
+
 void make_block(int fd) {
-	fcntl(fd, F_SETFL, ~O_NONBLOCK);
+	int flag;
+	if ((flag = fcntl(fd, F_GETFL)) < 0) {
+		return ;
+	}
+	flag &= ~O_NONBLOCK;
+	fcntl(fd, F_SETFL, flag);
 }
+/*
+这是一个函数，用于将文件描述符设置为阻塞模式。
+
+函数首先使用 fcntl 函数的 F_GETFL 选项来获取文件描述符的当前标志。如果调用失败，则函数返回。
+
+然后，它将 flag 变量与 ~O_NONBLOCK 常量进行按位与运算。O_NONBLOCK 常量是用于设置文件描述符为非阻塞模式的常量。 ~O_NONBLOCK 将 O_NONBLOCK 常量的每个位取反，然后与 flag 变量进行按位与运算。这将导致 flag 变量的 O_NONBLOCK 位被设置为 0。
+
+最后，函数使用 fcntl 函数的 F_SETFL 选项将修改后的 flag 值写回文件描述符。这将导致文件描述符被设置为阻塞模式。
+
+注意：使用 fcntl 函数可能是不可移植的，因此您可能需要使用其他方法来在不同的系统上设置文件描述符的阻塞模式。
+*/
